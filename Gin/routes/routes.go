d584f1b46b96be64a1313378d2ba8bb14be04a3e
Jerson Martínez <hotaka01@grupomrw.onmicrosoft.com>
2024-03-30 20:15:12 -0600

    Manejo de solicitudes HTTP

--- Gin/routes/routes.go
+++ Gin/routes/routes.go
@@ -6,6 +6,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Usuario struct {
+	Nombre string `json:"nombre"`
+	Email  string `json:"email"`
+}
+
+var usuarios []Usuario
+
 func SetupRoutes(r *gin.Engine) {
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "¡Hola, mundo!")
@@ -14,5 +21,22 @@ func SetupRoutes(r *gin.Engine) {
 	r.GET("/saludo/:nombre", func(c *gin.Context) {
 		nombre := c.Param("nombre")
 		c.String(http.StatusOK, "¡Hola, %s!", nombre)
+	})
+
+	r.POST("/usuarios", func(c *gin.Context) {
+		var nuevoUsuario Usuario
+
+		if err := c.BindJSON(&nuevoUsuario); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Error al decodificar el JSON"})
+			return
+		}
+
+		if nuevoUsuario.Nombre == "" || nuevoUsuario.Email == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Nombre y correo electrónico son campos requeridos"})
+		}
+
+		usuarios = append(usuarios, nuevoUsuario)
+
+		c.JSON(http.StatusOK, gin.H{"mensaje": "Usuario registrado", "datos": usuarios})
 	})
 }
