08590814ee3b00af957920d14d199071aa5d22c7
Jerson Martínez <hotaka01@grupomrw.onmicrosoft.com>
2024-03-30 21:12:47 -0600

    Adapatación de plantilla completa con diferentes páginas

--- Gin/routes/routes.go
+++ Gin/routes/routes.go
@@ -2,6 +2,8 @@ package routes
 
 import (
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -15,15 +17,26 @@ var usuarios []Usuario
 
 func SetupRoutes(r *gin.Engine) {
 
-	r.LoadHTMLGlob("templates/*")
+	r.Static("/static", "./static")
+
+	r.LoadHTMLGlob("templates/*.html")
 
 	r.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"Title":   "Mi aplicación",
-			"Heading": "¡Hola, mundo!",
-			"Message": "Bienvenido a mi aplicación web con Gin y plantillas HTML.",
-		})
+		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
-	r.Static("/static", "./static")
+	r.GET("/:page", func(c *gin.Context) {
+
+		page := c.Param("page")
+
+		if !strings.HasSuffix(page, ".html") {
+			page += ".html"
+		}
+
+		if _, err := os.Stat("templates/" + page); err == nil {
+			c.HTML(http.StatusOK, page, nil)
+		} else {
+			c.HTML(http.StatusNotFound, "404.html", nil)
+		}
+	})
 }
