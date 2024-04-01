5d237d6281a624024c76c889f991c2f897cef2b7
Jerson Martínez <hotaka01@grupomrw.onmicrosoft.com>
2024-04-01 00:19:54 -0600

    Adaptación de plantilla completa con diferentes páginas utilizando Echo

--- Echo/routes/routes.go
+++ Echo/routes/routes.go
@@ -4,25 +4,16 @@ import (
 	"html/template"
 	"io"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/labstack/echo/v4"
 )
 
-type TemplateRenderer struct {
-	templates *template.Template
-}
-
-func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
-
 func SetupRoutes(e *echo.Echo) {
 
-	renderer := &TemplateRenderer{
-		templates: template.Must(template.ParseGlob("templates/*.html")),
-	}
-
-	e.Renderer = renderer
+	e.Renderer = Renderer()
+	e.Static("/static", "static")
 
 	e.GET("/", func(c echo.Context) error {
 		return c.Render(http.StatusOK, "index.html", map[string]string{
@@ -30,6 +21,22 @@ func SetupRoutes(e *echo.Echo) {
 			"Heading": "¡Hola, mundo!",
 			"Message": "Bienvenido a mi aplicación web con Echo y plantillas HTML.",
 		})
+	})
+
+	e.GET("/:page", func(c echo.Context) error {
+
+		page := c.Param("page")
+
+		if !strings.HasSuffix(page, ".html") {
+			page += ".html"
+		}
+
+		if _, err := os.Stat("templates/" + page); err == nil {
+			return c.Render(http.StatusOK, page, nil)
+		}
+
+		return c.Render(http.StatusNotFound, "404.html", nil)
+
 	})
 
 	e.GET("/saludo", func(c echo.Context) error {
@@ -40,6 +47,18 @@ func SetupRoutes(e *echo.Echo) {
 		nombre := c.Param("nombre")
 		return c.String(http.StatusOK, "¡Hola, "+nombre+"!")
 	})
+}
 
-	e.Static("/static", "static")
+func Renderer() *TemplateRenderer {
+	return &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("templates/*.html")),
+	}
+}
+
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
 }
