b4a57e10451cd03c7674e876ab4682222f951649
Jerson Martínez <hotaka01@grupomrw.onmicrosoft.com>
2024-04-01 11:09:42 -0600

    Adaptación de plantilla completa con diferentes páginas utilizando Gorilla

--- Gorilla/main.go
+++ Gorilla/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"Gorilla/routes"
-	"html/template"
 	"log"
 	"net/http"
 	"time"
@@ -13,37 +12,12 @@ import (
 func main() {
 	r := mux.NewRouter()
 
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
-
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		renderTemplate(w, "index.html", map[string]interface{}{
-			"Title":   "Mi aplicación",
-			"Heading": "¡Hola, mundo!",
-			"Message": "Bienvenido a mi aplicación con Gorilla y plantillas HTML.",
-		})
-	})
-
 	routes.SetupRoutes(r)
 
 	r.Use(LogginMiddleware)
 
 	log.Println("Servidor Gorilla en ejecución en http://127.0.0.1:8080")
 	http.ListenAndServe(":8080", r)
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string, data interface{}) {
-	tmpl = "templates/" + tmpl
-	t, err := template.ParseFiles(tmpl)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-
-	err = t.Execute(w, data)
-
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
 }
 
 func LogginMiddleware(next http.Handler) http.Handler {
--- Gorilla/routes/routes.go
+++ Gorilla/routes/routes.go
@@ -1,20 +1,54 @@
 package routes
 
 import (
-	"fmt"
+	"html/template"
 	"net/http"
+	"os"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
 
 func SetupRoutes(r *mux.Router) {
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "¡Bienvenido al servidor Gorilla!")
-	})
+	renderer := Renderer()
 
-	r.HandleFunc("/saludo/{nombre}", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/", renderTemplate(renderer, "index.html"))
+
+	r.HandleFunc("/{page}", dynamicPageHandler(renderer)).Methods("GET")
+
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
+}
+
+func Renderer() *template.Template {
+	return template.Must(template.ParseGlob("templates/*.html"))
+}
+
+func renderTemplate(renderer *template.Template, templaName string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := renderer.ExecuteTemplate(w, templaName, nil)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+}
+
+func dynamicPageHandler(renderer *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		nombre := vars["nombre"]
-		w.Write([]byte("¡Hola, " + nombre + "!"))
-	})
+		page := vars["page"]
+
+		if !strings.HasSuffix(page, ".html") {
+			page += ".html"
+		}
+
+		if _, err := os.Stat("templates/" + page); err == nil {
+			err := renderer.ExecuteTemplate(w, page, nil)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+
+			return
+		}
+	}
 }
